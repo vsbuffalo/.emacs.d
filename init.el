b1cd1a1b4c61d434d59ef99134039e6962fe76ba
Vince Buffalo <vsbuffalo@ucdavis.edu>
2010-10-11 13:15:38 -0700

    removed block ELPA added to my init.el

--- init.el
+++ init.el
@@ -58,12 +58,3 @@
 (load-library "general")
 (load-library "visual")
 (load-library "mode-settings")
-;;; This was installed by package-install.el.
-;;; This provides support for the package system and
-;;; interfacing with ELPA, the package archive.
-;;; Move this code earlier if you want to reference
-;;; packages in your .emacs.
-(when
-    (load
-     (expand-file-name "~/.emacs.d/elpa/package.el"))
-  (package-initialize))
