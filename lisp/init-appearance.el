3d4f53aed9516a60930cb9ea10cbc7dfb19101da
Vince Buffalo <vsbuffaloAAAAAA@gmail.com>
2015-03-05 12:17:12 -0800

    fixed issue with solarized (see https://github.com/sellout/emacs-color-theme-solarized/issues/142)

--- lisp/init-appearance.el
+++ lisp/init-appearance.el
@@ -9,10 +9,16 @@
 (column-number-mode t)
 
 ;; Color theme
-(setq solarized-termcolors 256)
+(custom-set-variables '(solarized-termcolors 256))
 (load-theme 'solarized t)
-(setq background-mode 'dark)
-(enable-theme 'solarized)
 
+;; hack to fix this;
+;; https://github.com/jramnani/dotfiles/commit/da57ec2fd477d739a1a3597ccef4d96f4e35b504
+;; see: https://github.com/sellout/emacs-color-theme-solarized/issues/142
+(set-frame-parameter nil 'background-mode 'dark)
+(when (not (display-graphic-p))
+  (set-terminal-parameter nil 'background-mode 'dark))
+;; Call enable-theme to pick up the change to 'background-mode.
+(enable-theme 'solarized)
 
 (provide 'init-appearance)
